e6f3bad3692a5b9505efd8aa4214785105830b89
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2021-11-15 16:36:52 +0900

    FirstCommit

---
+++ TabBar_GUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBar_GUI
+//
+//  Created by 杉山悠己 on 2021/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
